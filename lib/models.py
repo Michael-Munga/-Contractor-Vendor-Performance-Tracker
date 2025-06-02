febf6ce5d87155f72b9931054d00d93bb6f2f839
Michael Munga <micknyalle@gmail.com>
2025-06-02 09:52:54 +0300

    initialize naming convention in models and create vendors table

--- lib/models.py
+++ lib/models.py
@@ -1,4 +1,23 @@
 from sqlalchemy.orm import declarative_base
+from sqlalchemy import Column,Integer,String,DateTime,MetaData
+from datetime import datetime
+# Naming convention
+convention = {
+    "ix": "ix_%(column_0_label)s",
+    "uq": "uq_%(table_name)s_%(column_0_name)s",
+    "ck": "ck_%(table_name)s_%(constraint_name)s",
+    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+    "pk": "pk_%(table_name)s"
+}
+meta = MetaData(naming_convention=convention)
 
+Base = declarative_base(metadata=meta)
+# vendor table
+class Vendor(Base):
+    __tablename__ = "vendors"
 
-Base = declarative_base()
+    id = Column(Integer, primary_key=True)
+    name = Column(String, nullable=False)
+    contact_info = Column(String)
+    specialty = Column(String)
+    created_at = Column(DateTime, default=datetime.now)
